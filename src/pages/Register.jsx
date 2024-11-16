0699ec0ab50eb77f3ba6e15e1d5ae13dfb575c3e
Charity <umercy001@gmail.com>
2024-11-16 21:21:42 +0100

    adjusted the paddings

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom"
 const Register = () => {
 
   return (
-<div className="m-10 border rounded-md p-8 shadow-lg bg-opacity-30 relative">
+<div className="m-10 border rounded-md p-6 shadow-lg bg-opacity-30 relative">
      <span><Link className="text-blue-700" to={'/other-options'}>Sign up with Single Sign-On (SSO)</Link> </span>
       <label className=" block text-gray-400">Email Addrress</label>
       <input className="block w-72 py-2.3 px-0 text-white bg-transparent border-2 rounded border-gray-200 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"  type="email" />
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,7 +6,7 @@ import { Link, useNavigate} from "react-router-dom"
 const SignIn = () => {
   const navigate = useNavigate();
   return (
-    <div className="m-10 text-slate-500 border rounded-md p-8 shadow-lg bg-opacity-30 relative">
+    <div className="m-10 text-slate-500 border rounded-md p-6 shadow-lg bg-opacity-30 relative">
       <label className="text-gray-400 ">Email Addrress</label>
       <input className="block w-72 py-2.3 px-0 text-white bg-transparent border-2 rounded border-gray-200 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"  type="email" />
       <button className="w-72 block mb-4 text-[18px]  mt-6 rounded-full bg-blue-600 text-white hover:bg-blue-800 py-2 transistion-colors duration-300" type="submit">Continue</button>
